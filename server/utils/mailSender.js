1ba2e60f5fd0453393af8ccd35f29b45f2782b3e
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-06-04 12:18:22 +0530

    add mailSender functionality in backend

---
+++ server/utils/mailSender.js
@@ -0,0 +1,28 @@
+const nodemailer = required("nodemailer");
+
+const mailSender = async (email,title,body) => {
+    try{
+            let transporter = nodemailer.createTransport({
+                host:process.env.MAIL_HOST,
+                auth:{
+                    user:process.env.MAIL_USER,
+                    pass:process.env.MAIL_PASS,
+                }
+            })
+
+            let info = await transporter.sendMail({
+                from:'CodeBoost - By Dhruvik Malaviya',
+                to:`${email}`,
+                subject:`${title}`,
+                html:`${body}`,
+            })
+
+            console.log(info);
+            return info;
+    }
+    catch(error){
+        console.log(error.message);
+    }
+}
+
+module.exports = mailSender;
