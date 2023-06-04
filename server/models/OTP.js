51cbd2c813492f37d6be5081808148eba3e3d38a
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-06-04 12:28:14 +0530

    add OTP send by Mail functionality in backend

--- server/models/OTP.js
+++ server/models/OTP.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
 
 const OTPSchema = new mongoose.Schema({
 
@@ -17,5 +18,23 @@ const OTPSchema = new mongoose.Schema({
     },
 
 });
+
+//a function -> to send emails
+async function sendVerificationEmail(email,otp){
+    try{
+        const mailResponse = await mailSender(email,"Verification Email from CodeBoost",otp);
+        console.log("Email sent successfully: ",mailResponse); 
+    }
+    catch(error){
+        console.log("error occured while sending mail: ",error);
+        throw error;
+    }
+}
+//pre middleware hook
+OTPSchema.pre("save",async function(next){
+    await sendVerificationEmail(this.email,this.otp);
+    next();
+})
+
 
 module.exports = mongoose.model("OTP",OTPSchema);
