6608e6b533d6d336bc55d8ad2e126fe7f293ca00
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-06-09 10:45:29 +0530

    fixed the bug while testing

--- server/controllers/RatingAndReview.js
+++ server/controllers/RatingAndReview.js
@@ -1,4 +1,4 @@
-const RatingAndReview = require("../models/RatingAndRaview");
+const RatingAndReview = require("../models/RatingAndReview");
 const Course = require("../models/Course");
 const { mongo, default: mongoose } = require("mongoose");
 
