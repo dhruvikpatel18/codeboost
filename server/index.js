020501f6c3bd6eb1edcf164fc55f274df73ed486
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-07-05 17:36:54 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -23,7 +23,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
 	cors({
-		origin:"http://localhost:3000",
+		origin:"https://codeboost-frontend-dhruvikpatel18.vercel.app/",
 		credentials:true,
 	})
 )
