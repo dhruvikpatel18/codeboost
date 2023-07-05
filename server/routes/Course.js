f400c1fcbe2ee41fee507493676ee742248ca8ca
Dhruvik Patel <dhruvikmalaviya1718@gmail.com>
2023-07-05 16:42:17 +0530

    Update Course.js

--- server/routes/Course.js
+++ server/routes/Course.js
@@ -35,7 +35,7 @@ const {
   createSubSection,
   updateSubSection,
   deleteSubSection,
-} = require("../controllers/Subsection")
+} = require("../controllers/SubSection")
 
 // Rating Controllers Import
 const {
